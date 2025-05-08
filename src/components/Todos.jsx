b8ff5a08eb28602fb0fbbf9c2fae3f18e8f9d5b6
siam114 <smsiamislam598@gmail.com>
2025-05-08 13:22:47 +0600

    shows todos

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getAllTodos } from '../services/actions/todosAction';
 
-export default function Todos() {
+const Todos = () => {
+    const {loading,todos,error} = useSelector((state) => state);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getAllTodos())
+    }, [dispatch])
   return (
     <div>
-      <h2>Todo-app</h2>
+      <h2>Todos App</h2>
+      {loading && <h3>Loading....</h3>}
+      {error && <h3>{error.message}</h3>}
+      <section>
+        {todos && todos.map((todo) =>{
+            return <article key={todo.id}>
+                <h4>{todo.id}</h4>
+                <h4>{todo.title}</h4>
+            </article>
+        })}
+      </section>
     </div>
   )
 }
+
+export default Todos
+
