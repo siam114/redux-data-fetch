90491ed547c009d086118d32be0455cabdcbab5c
siam114 <smsiamislam598@gmail.com>
2025-05-08 13:00:56 +0600

    create store

---
+++ src/store.js
@@ -0,0 +1,6 @@
+import { applyMiddleware, createStore } from "redux";
+import todosReducer from "./services/reducers/todosReducer";
+import { thunk } from "redux-thunk";
+
+const store = createStore(todosReducer, applyMiddleware(thunk));
+export default store;
